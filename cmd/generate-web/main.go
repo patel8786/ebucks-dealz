26c639cca73e800204642ce03e42f11bb76935fb
patel8786 <patel8786@gmail.com>
2021-10-28 14:01:09 +0200

    Update main.go

--- cmd/generate-web/main.go
+++ cmd/generate-web/main.go
@@ -18,7 +18,7 @@ func main() {
 	dataDirNameArg := flag.String("data-dir", "./data", "directory that contains scraped data files")
 	ouputDirArg := flag.String("output-dir", "docs", "data to write rendered HTML content to")
 	pagePathPrefixArg := flag.String("path-prefix", "", "prefix page link URLs (in case pages are hosted at a subpath); should start with '/'")
-        fmt.Println("XXXXXXXXXXXXXXXXXX TEST")
+        fmt.Println("XXXXXXXXXXXXXXXXXX dataDirNameArg=", dataDirNameArg)
 
 	flag.Parse()
 
