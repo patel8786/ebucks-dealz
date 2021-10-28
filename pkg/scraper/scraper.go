81865b065bf4db03470d03f9e32d0dd25c5954af
patel8786 <patel8786@gmail.com>
2021-10-28 20:46:12 +0200

    Update scraper.go

--- pkg/scraper/scraper.go
+++ pkg/scraper/scraper.go
@@ -27,8 +27,6 @@ func NewScraper(cacheDir string, threads int, callback ProductPageCallbackFunc) 
 	options := []colly.CollectorOption{
 		colly.AllowedDomains("www.ebucks.com"),
 		colly.URLFilters(
-			regexp.MustCompile(`https://www\.ebucks\.com/web/shop/shopHome\.do`),
-			regexp.MustCompile(`https://www\.ebucks\.com/web/shop/categorySelected\.do?catId=22767716.*`),
 			regexp.MustCompile(`https://www\.ebucks\.com/web/shop/productSelected\.do.*catId=22767716.*`),
 		),
 		colly.UserAgent("Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0"),
