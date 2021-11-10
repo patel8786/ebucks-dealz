9bf081d953abe39b89914263e46ac5d4fb4b7f83
patel8786 <patel8786@gmail.com>
2021-11-10 09:56:26 +0200

    Update scraper.go

--- pkg/scraper/scraper.go
+++ pkg/scraper/scraper.go
@@ -65,6 +65,17 @@ func NewScraper(cacheDir string, threads int, callback ProductPageCallbackFunc) 
 		if req.URL.String() == "https://www.ebucks.com/web/eBucks/errors/globalExceptionPage.jsp" {
 			return fmt.Errorf("not following redirect (implies error) %q : %+v", req.URL.String(), req.Header)
 		}
+		if req.URL.String() == "https://www.ebucks.com/web/eBucks" {
+			fmt.Fprintf(os.Stderr, "Redirect to Home page. Sleeping for 10 seconds. %s -> %s\n", via[0].URL.String(), req.URL.String())
+			if redirectErrors < 20 {
+				time.Sleep(10 * time.Second)
+				if err := s.visit(via[0].URL.String()); err != nil {
+					return err
+				}
+			} else {
+				return fmt.Errorf("Too many redirect errors %q : %+v", req.URL.String(), req.Header)
+			}
+		}
 		fmt.Fprintf(os.Stderr, "Redirecting %s -> %s\n", via[0].URL.String(), req.URL.String())
 
 		return nil
